8b1dc745cd75c9972f1cbbb4d202beb0f11b12a7
Shivjeet <79589597+shivjeetdyade@users.noreply.github.com>
2025-01-12 01:19:58 +0530

    Update Coffee_machine.py

--- Coffee_machine.py
+++ Coffee_machine.py
@@ -1,1 +1,19 @@
 
+#prompt user
+user_choice = input("What would you like? \nespresso\nlatte\ncappuccino\n\n\n)"  )
+
+#proceed as per user's choice
+
+if user_choice == "espresso":
+  pass
+elif user_choice == "latte":
+  pass
+elif user_choice == "cappuccino":
+  pass
+elif user_choice == "off":
+  pass
+elif user_choice == "report":
+  pass
+else:
+  print("Please opt for a valid option.")
+
