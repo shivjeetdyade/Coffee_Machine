04c770a9324b3bd292f663d4860cc072f96ebea5
Shivjeet <79589597+shivjeetdyade@users.noreply.github.com>
2025-01-12 00:59:55 +0530

    Update Coffee_machine.py

--- Coffee_machine.py
+++ Coffee_machine.py
@@ -1,1 +1,17 @@
+class Drink:
+    def __init__(self, name = '', price = 0.0, contents = []):
+        self.name = name
+        self.price = price
+        self.contents = contents
 
+    def __str__(self):
+        return f"name = {self.name}, price = {self.price}, contents = {self.contents}"
+
+espresso = Drink('espresso',2,['milk', 'sugar', 'water', 'coffee_beans'])
+
+print(espresso)
+
+def Prompt():
+    drink = input("What would you like? (espresso/latte/cappuccino): ")
+    if drink == "espresso":
+        pass
