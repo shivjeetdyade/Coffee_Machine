523b7a16663f4f1dfbb818d827d4330803e9873d
Shivjeet Dyade <shivjeet.dyade@datametica.com>
2025-01-14 01:57:49 +0530

    new coffee machine project

--- Coffee_machine.py
+++ Coffee_machine.py
@@ -1,19 +1,54 @@
+import sys
 
 #prompt user
 user_choice = input("What would you like? \nespresso\nlatte\ncappuccino\n\n\n)"  )
 
-#proceed as per user's choice
+water = 1000
+Milk = 1000
+Coffee = 1000
+Money = 1000
+
+#proceed as per user's choice #check if resources are available
 
 if user_choice == "espresso":
-  pass
+    if not water >= 100:
+        print("Sorry, There's not enough Water")
+    elif not Milk >= 100:
+        print("Sorry, There's not enough Milk")
+    elif not water >= 100:
+        print("Sorry, There's not enough Water")
+    else:
+        print("Please insert coins")
+        input("quarters
+        input("dimesnickles = $0.05, pennies = $0.01)
+
 elif user_choice == "latte":
-  pass
+    if not water >= 50:
+        print("Sorry, There's not enough Water")
+    elif not Milk >= 50:
+        print("Sorry, There's not enough Milk")
+    elif not water >= 50:
+        print("Sorry, There's not enough Water")
+
 elif user_choice == "cappuccino":
-  pass
+    if not water >= 150:
+        print("Sorry, There's not enough Water")
+    elif not Milk >= 150:
+        print("Sorry, There's not enough Milk")
+    elif not water >= 150:
+        print("Sorry, There's not enough Water")
+
 elif user_choice == "off":
-  pass
+  #turn off the machine
+    print("Machine has stopped.")
+    sys.exit(0)
+
 elif user_choice == "report":
-  pass
+    print(f"{Water : } ml")
+    print(f"{Milk: } ml")
+    print(f"{Coffee: } g")
+    print(f"{Money: $}")
+
 else:
   print("Please opt for a valid option.")
 
